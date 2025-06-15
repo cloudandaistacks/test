94ef02a62a5b752c9f30bc13be6e7e19a4a3deb9
cloudandaistacks <johndoe@example.com>
2025-06-15 21:25:35 +0530

    creating s3 bucket

--- main.tf
+++ main.tf
@@ -5,27 +5,31 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
 
-  owners = ["719108377761"] # Canonical
-}
+#   owners = ["719108377761"] # Canonical
+# }
 
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+# resource "aws_instance" "ubuntu" {
+#   ami           = data.aws_ami.ubuntu.id
+#   instance_type = var.instance_type
 
-  tags = {
-    Name = var.instance_name
-  }
+#   tags = {
+#     Name = var.instance_name
+#   }
+# }
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "cloudandaistacks-demo"
 }
